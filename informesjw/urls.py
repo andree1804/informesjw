87354e85b9b032d59e65afcc7ab85a9fc9b681b2
Erick Sanchez <erick.sanchez@comercio.com.pe>
2025-06-20 07:28:53 -0500

    Agrego vista básica en la raízxxxx

--- informesjw/urls.py
+++ informesjw/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("<h1>¡La app Django está corriendo correctamente en Railway!</h1>")
