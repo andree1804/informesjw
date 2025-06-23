5a73d0fd60e587125735d96dc84c18ae9c9137ae
Erick Sanchez <erick.sanchez@comercio.com.pe>
2025-06-23 10:57:44 -0500

    mejora1

--- informesjw/urls.py
+++ informesjw/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+admin.site.site_header = "Panel de Administración"
+admin.site.site_title = "Panel de Administración"
+admin.site.index_title = "Panel de Administración"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
